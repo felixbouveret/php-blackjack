d85b4efa4746de22edff2df9e9e9854b4fbe64c0
Benoit Lamonica <ben33127@gmail.com>
2020-11-19 17:19:41 +0100

    Console Handler

--- run.php
+++ run.php
@@ -1,6 +1,10 @@
 <?php
 
-use src\classes\Cards;
+use src\classes\Deck;
+use src\classes\Console;
 
 // REQUIRES
 require_once 'vendor/autoload.php';
+
+$consoleHandler = new Console;
+echo 'Tu veux ' . $consoleHandler->ask('Combien denfant tu veux ?', [1, 2, 3, 4]) . ' enfant(s)';
