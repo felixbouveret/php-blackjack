245fbab0a6b240eff5bfc9a47e99e4554bf9b24b
Benoit Lamonica <ben33127@gmail.com>
2020-11-19 16:18:02 +0100

    Cards classes

--- run.php
+++ run.php
@@ -4,13 +4,3 @@ use src\classes\Cards;
 
 // REQUIRES
 require_once 'vendor/autoload.php';
-
-$cards = new Cards;
-$cards->shuffle();
-$cards->pick()->printName();
-$cards->pick()->printName();
-$cards->pick()->printName();
-$cards->pick()->printName();
-$cards->pick()->printName();
-$cards->pick()->printName();
-$cards->printTrash();
